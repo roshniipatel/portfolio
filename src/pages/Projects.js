e8a427957669907e44deb700eee54473cfce1745
roshniipatel <roshnicpatel00@gmail.com>
2023-06-16 11:58:18 -0500

    added project 3

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import './Projects.css';
+import dateItineraryImage from '../assets/images/date-itinerary.png';
+import onTrackImage from '../assets/images/on-track.png';
+import quellImage from '../assets/images/quell.png';
+import weatherAppImage from '../assets/images/weather-app.png';
 
 const Projects = () => {
   const projects = [
     {
       title: 'Date Itinerary',
-      image: '../assets/images/date-itinerary.png',
-      description: 'This website caters to individuals in romantic relationships who face challenges in planning dates, either due to their indecisiveness or lack of experience in organizing a well-crafted itinerary.'
+      image: dateItineraryImage,
+      description: 'This website caters to individuals in romantic relationships who face challenges in planning dates, either due to their indecisiveness or lack of experience in organizing a well-crafted itinerary.',
+      link: 'https://date-itinerary.herokuapp.com/'
     },
     {
       title: 'On Track',
-      image: '../assets/images/on-track.png',
-      description: 'This application caters to individuals dedicated to cultivating a more health-conscious lifestyle.'
+      image: onTrackImage,
+      description: 'This application caters to individuals dedicated to cultivating a more health-conscious lifestyle.',
+      link: 'https://on-track.herokuapp.com/'
+    },
+    {
+      title: 'Quell',
+      image: quellImage,
+      description: 'A supportive and empowering community platform dedicated to fostering mental well-being and personal growth.',
+      link: 'https://quell-mental-health-9f784a2642a5.herokuapp.com/'
     },
     {
       title: 'Weather Application',
-      image: '../assets/images/weather-app.png',
-      description: 'An application that shows you the (5 days) temperature for any city that you search for.'
+      image: weatherAppImage,
+      description: 'An application that shows you the (5 days) temperature for any city that you search for.',
+      link: 'https://roshniipatel.github.io/how-is-the-weather/'
     }
   ];
 
@@ -27,8 +40,8 @@ const Projects = () => {
         {projects.map((project, index) => (
           <div className="project-item" key={index}>
             <img src={project.image} alt={project.title} />
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
+            <h3><a href={project.link}>{project.title}</a></h3>
+            <p className="description">{project.description}</p>
           </div>
         ))}
       </div>
@@ -36,4 +49,6 @@ const Projects = () => {
   );
 };
 
+
 export default Projects;
+
